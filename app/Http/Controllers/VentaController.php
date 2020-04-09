898cd0af2edbd911a150d95955eabbdf59d76cb0
Reydem <reydem613@gmail.com>
2020-04-09 08:45:20 -0500

    104 PROYECTO FINAL  Gestion de ventas Minuto: final

--- app/Http/Controllers/VentaController.php
+++ app/Http/Controllers/VentaController.php
@@ -66,8 +66,7 @@ class VentaController extends Controller
 
         $id = $request->id;
         $detalles = DetalleVenta::join('articulos','detalle_ventas.idarticulo','=','articulos.id')
-        ->select('detalle_ventas.cantidad','detalle_ventas.precio','detalle_ventas.descuento',
-        'articulos.nombre as articulo')
+        ->select('detalle_ventas.cantidad','detalle_ventas.precio','detalle_ventas.descuento','articulos.nombre as articulo')
         ->where('detalle_ventas.idventa','=',$id)
         ->orderBy('detalle_ventas.id', 'desc')->get();
         
